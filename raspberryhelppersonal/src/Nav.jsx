bb87f639c895f022a5424d0344d6503569f7f8c9
raspberryhelp <agnus1603@gmail.com>
2026-09-09 21:30:41 -0500

    added a hover sound and touched the navigation bar thing

--- raspberryhelppersonal/src/Nav.jsx
+++ raspberryhelppersonal/src/Nav.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Link, Links } from "react-router-dom";
-function Nav() {
-  return (
-    <div class = "navigation-bar">
-        <Link to ="/">About</Link>
-        <Link to ="/Projects">Projects</Link>
-        <Link to ="/Tools">Tools</Link>
-        <Link to ="/Blog">Blog</Link>
-        <Link to ="/Art">Art</Link>
-        <Link to ="/Widgets">Widgets</Link>
+import click from './assets/navtouch.wav';
 
+function Nav() {
+
+
+  const playClickSound = () => {
+    const audio = new Audio(click);
+    audio.volume = 0.3;
+    audio.play();
+};
+
+  return (
+    <div className="navigation-bar">
+      <Link to="/"onClick={playClickSound}>About</Link>
+      <Link to="/Projects"  onClick={playClickSound}>Projects</Link>
+      <Link to="/Tools"  onClick={playClickSound}>Tools</Link>
+      <Link to="/Blog"  onClick={playClickSound}>Blog</Link>
+      <Link to="/Art" onClick={playClickSound}>Art</Link>
+      <Link to="/Widgets" onClick={playClickSound}>Widgets</Link>
     </div>
   )
 }
