264133b3cc96994aa4f3cea99e45518784768d0e
raspberryhelp <agnus1603@gmail.com>
2026-09-09 20:38:11 -0500

    Overhauled it a lot, took some suggestions from claude and as awful as it to say i like how it looks. might change the background color

--- raspberryhelppersonal/src/App.jsx
+++ raspberryhelppersonal/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation  } from 'react-router-dom';
 import './App.css'
 import Nav from "./Nav";
 import About from "./pages/About";
@@ -11,31 +11,38 @@ import Tools from "./pages/Tools";
 import Blog from "./pages/Blog";
 import Art from "./pages/Art";
 import Widgets from "./pages/Widgets"; 
+function PageContent() {
+  const location = useLocation();
 
+  return (
+    <div className="main-content" key={location.pathname}>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Tools" element={<Tools />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Art" element={<Art />} />
+        <Route path="/Widgets" element={<Widgets />} />
+      </Routes>
+    </div>
+  );
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <body>
+  
       <BrowserRouter>
       <div class = "container">
         <Nav></Nav>
         <div class = "main-content">
-         <Routes>
-            <Route path = "/" element={<About/>}/>
-            <Route path = "/Projects" element={<Projects/>}/>
-            <Route path = "/Tools" element={<Tools/>}/>
-            <Route path = "/Blog" element={<Blog/>}/>
-            <Route path = "/Art" element={<Art/>}/>
-            <Route path = "/Widgets" element={<Widgets/>}/>
-            
-          </Routes>
+          <PageContent />
         </div>
       </div>
       </BrowserRouter>
     
-    </body>
+
       
     </>
   )
--- raspberryhelppersonal/src/pages/About.jsx
+++ raspberryhelppersonal/src/pages/About.jsx
@@ -3,11 +3,27 @@ import React from 'react'
 function About() {
   return (
     <div>
-      Masters CS Student, studying Interactive Computing along with Data Science. Studies at the University of Texas at Dallas.
-      This site is a WIP. 
-      
+      <div class = "about-container">
+        <p style={{width: '250px', height: '250px', margin: '50px'}}>
+          <img src="https://i.pinimg.com/736x/1f/9f/68/1f9f68a53a9e39df3ee4067c823efd6b.jpg" width="250px"></img>
+        </p>
+        <p style={{width: '250px', height: '250px', margin:'50px', alignItems:'center'}}> 
+          ❖ Agnus Thomas<br/>
+          ❖ Dallas, Texas<br/>
+          ❖ Graduate student studying Computer Science at University of Texas at Dallas<br/>
+          ❖ Specializes in Data Science, aims for Fullstack<br/>
+       </p>
+      </div>
+
+      <div class = "about-container">
+        <p>
+          This site is mostly just a portfolio of my stuff, feel free to take a look around. This site is a WIP so there is nothing. It also looks basic because I haven't taken the time to like make it cooler idk
+        </p>
+      </div>
+
     </div>
   )
 }
 
 export default About
+
