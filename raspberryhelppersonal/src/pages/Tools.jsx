c590fed76a99265626d60366783329195f446a43
raspberryhelp <agnus1603@gmail.com>
2026-09-20 18:04:12 -0500

    cool new gradient to my website

--- raspberryhelppersonal/src/pages/Tools.jsx
+++ raspberryhelppersonal/src/pages/Tools.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+const toolset = [{pname:"Languages", info:"Python, PHP, C, C++, Java, SQL (MySQL and SQLAlchemy), JavaScript"},
+  {pname:"Developer Tools", info:"Visual Studio Code, CLion, IntelliJ, PyCharm"}, 
+  {pname:"Operating Systems", info:"Windows 11, Ubuntu"},
+  {pname:"Libraries", info:"Flask, Librosa, NumPy, React, Tensorflow"},
+]
+const Card = ({ card, index }) => (
+  <div className="toolCard" key = {index}>
+    
+      <div className="toolTitle">{card.pname}</div>
+      <p className='toolContent'>{card.info}</p>
+    
+  </div>
+);
 function Tools() {
   return (
-    <div>A list of my tools or something thats supposed to go here</div>
+    <div class="about-container">
+      <div>
+    {toolset.map((card, index) => (
+        <Card card={card} key={index} />
+      ))}
+  </div>
+    </div>
   )
 }
 
